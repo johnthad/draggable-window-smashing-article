ad24c647e04dbafdadf00a59239888031a21a2d1
Andrico <andrico.karoulla@healthhero.com>
2022-08-07 14:16:38 +0100

    set up article

---
+++ a2k-broken-window.js
@@ -0,0 +1,2 @@
+import { html, css, LitElement } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
--- a2k-window.js
+++ a2k-window.js
@@ -1,51 +1,10 @@
 import { html, css, LitElement } from "lit";
-import { property } from "lit/decorators.js";
 import { styleMap } from "lit/directives/style-map.js";
 import { DragController } from "./dragController";
 
-// import "@a2000/panel/a2k-panel.js";
-
 export class A2kWindow extends LitElement {
-  static styles = css`
-    :host {
-      font-family: var(--font-primary);
-    }
-
-    #window {
-      position: absolute;
-      width: 80ch;
-      max-width: 100%;
-    }
-
-    #draggable {
-      width: 100%;
-      position: relative;
-    }
-
-    [data-dragging="idle"] {
-      cursor: grab;
-    }
-
-    [data-dragging="dragging"] {
-      cursor: grabbing;
-    }
-  `;
-
-  drag = new DragController(this, {
-    containerId: "#window",
-  });
-
   render() {
-    return html`
-      <div id="window" style=${styleMap(this.drag.styles)}>
-        <a2k-panel>
-          <div id="draggable" ${this.drag.applyDrag()}>
-            <p>${this.heading}</p>
-          </div>
-          <slot></slot>
-        </a2k-panel>
-      </div>
-    `;
+    return html``;
   }
 }
 
