c83b5f4336fa972484f65e0e1107bdeb4cee1564
Andrico <andrico.karoulla@healthhero.com>
2022-08-16 19:38:57 +0100

    update start

--- dragController.js
+++ dragController.js
@@ -1,7 +1,3 @@
-import { noChange } from "lit";
-import { Directive, directive } from "lit/directive.js";
 import PointerTracker from "pointer-tracker";
-
-class DragDirective extends Directive {}
 
 export class DragController {}
