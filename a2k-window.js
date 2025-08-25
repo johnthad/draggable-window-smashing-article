32950126043f3e87eb98ab46e6eb234ea035ccfe
Thad Humphries <thad.humphries@gmail.com>
2025-08-25 09:31:04 -0400

    demo: complete per Smashing Magazine article

--- a2k-window.js
+++ a2k-window.js
@@ -1,9 +1,79 @@
-import { html, css, LitElement } from "lit";
-import { styleMap } from "lit/directives/style-map.js";
-import { DragController } from "./dragController";
+import { html, css, LitElement } from 'lit';
+import { styleMap } from 'lit/directives/style-map.js';
+import { DragController } from './dragController';
 
 class A2kWindow extends LitElement {
+  static styles = css`
+    :host {
+      font-family: var(--font-primary);
+    }
+
+    #window {
+      width: min(80ch, 100%);
+    }
+
+    #panel {
+      border: var(--border-width) solid var(--color-gray-400);
+      box-shadow: 2px 2px var(--color-black);
+      background-color: var(--color-gray-500);
+    }
+
+    #draggable {
+      background: linear-gradient(
+        90deg,
+        var(--color-blue-100) 0%,
+        var(--color-blue-700) 100%
+      );
+      user-select: none;
+    }
+
+    #draggable p {
+      font-weight: bold;
+      margin: 0;
+      color: white;
+      padding: 2px 8px;
+    }
+
+    [data-dragging='idle'] {
+      cursor: grab;
+    }
+
+    [data-dragging='dragging'] {
+      cursor: grabbing;
+    }
+  `;
+
+  static properties = {
+    heading: {},
+  };
+
+  constructor() {
+    super();
+    this.heading = 'Building Retro Web Components with Lit';
+  }
+
+  drag = new DragController(this, {
+    getContainerEl: () => this.shadowRoot.querySelector('#window'),
+    getDraggableEl: () => this.getDraggableEl(),
+  });
+
+  async getDraggableEl() {
+    await this.updateComplete;
+    return this.shadowRoot.querySelector('#draggable');
+  }
+
   render() {
-    return html`<slot></slot>`;
+    return html`
+      <div id="window" style=${styleMap(this.drag.styles)}>
+        <div id="panel">
+          <div id="draggable" data-dragging=${this.drag.state}>
+            <p>${this.heading}</p>
+          </div>
+          <slot></slot>
+        </div>
+      </div>
+    `;
   }
 }
+
+customElements.define('a2k-window', A2kWindow);
--- dragController.js
+++ dragController.js
@@ -1,3 +1,134 @@
-import PointerTracker from "pointer-tracker";
+import PointerTracker from 'pointer-tracker';
 
-export class DragController {}
+export class DragController {
+  x = 0;
+  y = 0;
+  state = 'idle';
+
+  styles = {
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+  };
+
+  constructor(host, options) {
+    const {
+      getContainerEl = () => null,
+      getDraggableEl = () => Promise.resolve(null),
+    } = options;
+
+    this.host = host;
+    this.host.addController(this);
+    this.getContainerEl = getContainerEl;
+
+    getDraggableEl().then(el => {
+      if (!el) return;
+
+      this.draggableEl = el;
+      this.init();
+    });
+  }
+
+  init() {
+    this.pointerTracker = new PointerTracker(this.draggableEl, {
+      start: (...args) => {
+        this.onDragStart(...args);
+        this.state = 'dragging';
+        this.host.requestUpdate();
+        return true;
+      },
+      move: (...args) => {
+        this.onDrag(...args);
+      },
+      end: (...args) => {
+        this.state = 'idle';
+        this.host.requestUpdate();
+      },
+    });
+  }
+
+  hostDisconnected() {
+    if (this.pointerTracker) {
+      this.pointerTracker.stop();
+    }
+  }
+
+  onDragStart = (pointer, ev) => {
+    this.cursorPositionX = Math.floor(pointer.pageX);
+    this.cursorPositionY = Math.floor(pointer.pageY);
+  };
+
+  onDrag = (_, pointers) => {
+    this.calculateWindowPosition(pointers[0]);
+  };
+
+  calculateWindowPosition(pointer) {
+    const el = this.draggableEl;
+    const containerEl = this.getContainerEl();
+
+    if (!el || !containerEl) return;
+
+    const oldX = this.x;
+    const oldY = this.y;
+
+    //JavaScript’s floats can be weird, so we’re flooring these to integers.
+    const parsedTop = Math.floor(pointer.pageX);
+    const parsedLeft = Math.floor(pointer.pageY);
+
+    //JavaScript’s floats can be weird, so we’re flooring these to integers.
+    const cursorPositionX = Math.floor(pointer.pageX);
+    const cursorPositionY = Math.floor(pointer.pageY);
+
+    const hasCursorMoved =
+      cursorPositionX !== this.cursorPositionX ||
+      cursorPositionY !== this.cursorPositionY;
+
+    // We only need to calculate the window position if the cursor position has changed.
+    if (hasCursorMoved) {
+      const { bottom, height } = el.getBoundingClientRect();
+      const { right, width } = containerEl.getBoundingClientRect();
+
+      // The difference between the cursor’s previous position and its current position.
+      const xDelta = cursorPositionX - this.cursorPositionX;
+      const yDelta = cursorPositionY - this.cursorPositionY;
+
+      // The happy path - if the element doesn’t attempt to go beyond the browser’s boundaries.
+      this.x = oldX + xDelta;
+      this.y = oldY + yDelta;
+
+      const outOfBoundsTop = this.y < 0;
+      const outOfBoundsLeft = this.x < 0;
+      const outOfBoundsBottom = bottom + yDelta > window.innerHeight;
+      const outOfBoundsRight = right + xDelta >= window.innerWidth;
+
+      const isOutOfBounds =
+        outOfBoundsBottom ||
+        outOfBoundsLeft ||
+        outOfBoundsRight ||
+        outOfBoundsTop;
+
+      // Set the cursor positions for the next time this function is invoked.
+      this.cursorPositionX = cursorPositionX;
+      this.cursorPositionY = cursorPositionY;
+
+      // Otherwise, we force the window to remain within the browser window.
+      if (outOfBoundsTop) {
+        this.y = 0;
+      } else if (outOfBoundsLeft) {
+        this.x = 0;
+      } else if (outOfBoundsBottom) {
+        this.y = window.innerHeight - height;
+      } else if (outOfBoundsRight) {
+        this.x = Math.floor(window.innerWidth - width);
+      }
+
+      this.updateElPosition();
+      // We trigger a lifecycle update.
+      this.host.requestUpdate();
+    }
+  }
+
+  updateElPosition(x, y) {
+    this.styles.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
+}
